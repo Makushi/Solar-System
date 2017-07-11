a8bc9565cbf0ece213744eeff445ad58664f4563
Makushi <maxivicraba@hotmail.com>
2017-07-10 21:35:53 -0300

    Add files via upload

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+	public float speed;
+	public Transform pivotPoint;
+
+	void Update () {
+		transform.RotateAround (pivotPoint.position, Vector3.up, speed);
+	}
+}
